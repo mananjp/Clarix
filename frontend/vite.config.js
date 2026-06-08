a86427cc9d5dae0de75d2affbdd60e44c8476c8b
Tirth Patel <tbhanderi872@rku.ac.in>
2026-06-08 17:04:20 +0530

    fix: add vercel rewrites

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,14 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: '../app/static',
-    emptyOutDir: true,
-  },
   server: {
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:8000',
+        target: 'https://sfdr-backend.onrender.com',
         changeOrigin: true,
       },
     },
